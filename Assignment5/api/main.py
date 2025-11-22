05e1d6e603c0cec4b1c1856914710f2b7705ba88
Mason Kuykendall <95830250+TheBotManMason@users.noreply.github.com>
2025-11-21 23:49:01 -0500

    Added assignment 5

---
+++ Assignment5/api/main.py
@@ -0,0 +1,121 @@
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
+
+from .models import models, schemas
+from .controllers import orders, sandwiches, recipes, resources
+from .dependencies.database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/orders/", response_model=schemas.Order, tags=["Orders"])
+def create_order(order: schemas.OrderCreate, db: Session = Depends(get_db)):
+    return orders.create(db=db, order=order)
+
+
+@app.get("/orders/", response_model=list[schemas.Order], tags=["Orders"])
+def read_orders(db: Session = Depends(get_db)):
+    return orders.read_all(db)
+
+
+@app.get("/orders/{order_id}", response_model=schemas.Order, tags=["Orders"])
+def read_one_order(order_id: int, db: Session = Depends(get_db)):
+    order = orders.read_one(db, order_id=order_id)
+    if order is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return order
+
+
+@app.put("/orders/{order_id}", response_model=schemas.Order, tags=["Orders"])
+def update_one_order(order_id: int, order: schemas.OrderUpdate, db: Session = Depends(get_db)):
+    order_db = orders.read_one(db, order_id=order_id)
+    if order_db is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return orders.update(db=db, order=order, order_id=order_id)
+
+
+@app.delete("/orders/{order_id}", tags=["Orders"])
+def delete_one_order(order_id: int, db: Session = Depends(get_db)):
+    order = orders.read_one(db, order_id=order_id)
+    if order is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return orders.delete(db=db, order_id=order_id)
+
+
+# SANDWICHES
+@app.get("/sandwiches", tags=["Sandwiches"])
+def get_sandwiches(db: Session = Depends(get_db)):
+    return sandwiches.read_all(db)
+
+@app.get("/sandwiches/{sandwich_id}", tags=["Sandwiches"])
+def get_sandwich(sandwich_id: int, db: Session = Depends(get_db)):
+    return sandwiches.read_one(db, sandwich_id)
+
+@app.post("/sandwiches", tags=["Sandwiches"])
+def create_sandwich(sandwich: schemas.SandwichCreate, db: Session = Depends(get_db)):
+    return sandwiches.create(db, sandwich)
+
+@app.put("/sandwiches/{sandwich_id}", tags=["Sandwiches"])
+def update_sandwich(sandwich_id: int, sandwich: schemas.SandwichUpdate, db: Session = Depends(get_db)):
+    return sandwiches.update(db, sandwich_id, sandwich)
+
+@app.delete("/sandwiches/{sandwich_id}", tags=["Sandwiches"])
+def delete_sandwich(sandwich_id: int, db: Session = Depends(get_db)):
+    return sandwiches.delete(db, sandwich_id)
+
+
+# RESOURCES
+@app.get("/resources", tags=["Resources"])
+def get_resources(db: Session = Depends(get_db)):
+    return resources.read_all(db)
+
+@app.get("/resources/{resource_id}", tags=["Resources"])
+def get_resource(resource_id: int, db: Session = Depends(get_db)):
+    return resources.read_one(db, resource_id)
+
+@app.post("/resources", tags=["Resources"])
+def create_resource(resource: schemas.ResourceCreate, db: Session = Depends(get_db)):
+    return resources.create(db, resource)
+
+@app.put("/resources/{resource_id}", tags=["Resources"])
+def update_resource(resource_id: int, resource: schemas.ResourceUpdate, db: Session = Depends(get_db)):
+    return resources.update(db, resource_id, resource)
+
+@app.delete("/resources/{resource_id}", tags=["Resources"])
+def delete_resource(resource_id: int, db: Session = Depends(get_db)):
+    return resources.delete(db, resource_id)
+
+
+# RECIPES
+@app.get("/recipes", tags=["Recipes"])
+def get_recipes(db: Session = Depends(get_db)):
+    return recipes.read_all(db)
+
+@app.get("/recipes/{recipe_id}", tags=["Recipes"])
+def get_recipe(recipe_id: int, db: Session = Depends(get_db)):
+    return recipes.read_one(db, recipe_id)
+
+@app.post("/recipes", tags=["Recipes"])
+def create_recipe(recipe: schemas.RecipeCreate, db: Session = Depends(get_db)):
+    return recipes.create(db, recipe)
+
+@app.put("/recipes/{recipe_id}", tags=["Recipes"])
+def update_recipe(recipe_id: int, recipe: schemas.RecipeUpdate, db: Session = Depends(get_db)):
+    return recipes.update(db, recipe_id, recipe)
+
+@app.delete("/recipes/{recipe_id}", tags=["Recipes"])
+def delete_recipe(recipe_id: int, db: Session = Depends(get_db)):
+    return recipes.delete(db, recipe_id)
